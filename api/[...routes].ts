8b7bf116d6dd63670c96c9f27eb9a11f58546da2
Emir Risyad <emirrsyd@gmail.com>
2025-09-07 21:36:26 +0200

    refactor: switch to Vercel API routes for deployment

--- api/[...routes].ts
+++ api/[...routes].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/app.js';
+import app from '../src/app';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
